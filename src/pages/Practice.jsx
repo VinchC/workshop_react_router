644e55dcc414f3dbdf5edbd87c60c9dd57be3d8f
Vincent C <vinch.cm@gmail.com>
2024-03-07 11:16:24 +0100

    two arrays added with map to display their content in the page /practice

--- src/pages/Practice.jsx
+++ src/pages/Practice.jsx
@@ -1,10 +1,32 @@
 import { useRouteLoaderData } from "react-router-dom";
 
 function Practice() {
+  const myArray = [1, 2, 3, 4];
+  const myArrayObject = [
+    { userName: "Pops", age: "42" },
+    { userName: "Machin", age: "36" },
+  ];
   const helloMachin = useRouteLoaderData("practice");
   const helloPops = () => "Hello Pops";
   return (
     <>
+      {myArray.map((item) => {
+        return (
+          <>
+            <p>{item}</p>
+          </>
+        );
+      })}
+      <ul>
+        {myArrayObject.map((item) => {
+          return (
+            <>
+              <li>{item.userName}</li>
+              <li>{item.age}</li>
+            </>
+          );
+        })}
+      </ul>
       <h1>Hello from Practice</h1>
       <p>{helloPops()}</p>
       <p>{helloMachin}</p>
